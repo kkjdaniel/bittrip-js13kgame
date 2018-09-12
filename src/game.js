abb071255f5a973f8f7b53ec120e539e2c4dac34
KKJDaniel <kkjdaniel@yahoo.co.uk>
2018-09-12 01:11:38 +0100

    Refactor

--- src/game.js
+++ src/game.js
@@ -1,182 +1,219 @@
-var context = document.getElementById('game').getContext('2d');
+var context = document.getElementById("game").getContext("2d");
 
-var GAME = {
-  STATES: {
-    MENU: 0,
-    PLAY: 1,
-    RESTART: 2
-  },
-  STATE: 0,
-  SCORE: 0
-};
+const HIT_LEEWAY = 4;
 
-var DIRECTIONS = {
+const DIRECTIONS = {
   UP: 0,
   DOWN: 1
-}
+};
 
-var player = {};
+const STATES = {
+  MENU: 0,
+  PLAY: 1,
+  RESTART: 2
+};
 
-var getNextTile = (function () {
+var GAME = {
+  STATE: STATES.MENU,
+  SCORE: 0,
+  PLAYER: {
+    height: 10,
+    width: 10
+  }
+};
+
+let getNextTile = (function() {
   var height = 100;
-  return function () {
-    Math.round(Math.random()) ? height < context.canvas.height / 2 && (height += 10) : height > 60 && (height -= 10);
+  return function() {
+    Math.round(Math.random())
+      ? height < context.canvas.height / 2 && (height += 10)
+      : height > 60 && (height -= 10);
     return {
       height: height,
       offset: Math.random() * (height * 0.6 - 0) + 0
     };
-  }
+  };
 })();
 
-var map = {
-  tiles: null,
-  nextStep: function () {
+let map = {
+  getNextTile: (function() {
+    var height = 100;
+    return function() {
+      Math.round(Math.random())
+        ? height < context.canvas.height / 2 && (height += 10)
+        : height > 60 && (height -= 10);
+      return {
+        height: height,
+        offset: Math.random() * (height * 0.6 - 0) + 0
+      };
+    };
+  })(),
+  nextStep: function() {
     this.tiles.shift();
-    this.tiles.push(getNextTile());
-  }
-}
-
-function step() {
-  map.nextStep();
-  GAME.SCORE = GAME.SCORE + 1;
-  if (GAME.STATE === GAME.STATES.PLAY) {
-    setTimeout(step, 25);
+    this.tiles.push(this.getNextTile());
   }
 };
 
+function gameTick() {
+  map.nextStep();
+  GAME.SCORE = GAME.SCORE + 1;
+  if (GAME.STATE === STATES.PLAY) {
+    setTimeout(gameTick, 25);
+  }
+}
+
 function initGame() {
-  map.tiles = Array.from(Array(50), function () {
+  map.tiles = Array.from(Array(50), function() {
     return {
       height: 100,
       offset: 0
     };
   });
-  player = {
+  GAME.PLAYER = Object.assign({}, GAME.PLAYER, {
     x: Math.ceil(context.canvas.width / map.tiles.length),
-    y: context.canvas.height / 2 - 10 / 2,
+    y: context.canvas.height / 2 - GAME.PLAYER.height / 2,
     direction: null
-  };
+  });
   GAME.SCORE = 0;
-  step();
+  gameTick();
 }
 
-function update(progress) {
-  if (GAME.STATE === GAME.STATES.PLAY) {
-    switch (player.direction) {
-      case DIRECTIONS.UP:
-        player.y = player.y - 2;
-        break;
-      case DIRECTIONS.DOWN:
-        player.y = player.y + 2;
+function playConfirmationSound() {
+  with (new AudioContext())
+    with ((G = createGain()))
+      for (i in (D = [5, 7]))
+        with (createOscillator())
+          if (D[i])
+            connect(G),
+              G.connect(destination),
+              start(i * 0.1),
+              frequency.setValueAtTime(440 * 1.06 ** (13 - D[i]), i * 0.1),
+              gain.setValueAtTime(1, i * 0.1),
+              gain.setTargetAtTime(0.0001, i * 0.1 + 0.08, 0.005),
+              stop(i * 0.1 + 0.09);
+}
+
+window.addEventListener("keydown", this.check, false);
+function check(e) {
+  if (GAME.STATE === STATES.MENU) {
+    switch (e.keyCode) {
+      case 32:
+        playConfirmationSound();
+        GAME.STATE = STATES.PLAY;
+        initGame();
+        window.requestAnimationFrame(loop);
         break;
     }
-    let startBoundary = (context.canvas.height / 2 - map.tiles[1].height / 2 + map.tiles[1].offset);
+  } else if (GAME.STATE === STATES.PLAY) {
+    switch (e.keyCode) {
+      case 38:
+        GAME.PLAYER.direction = DIRECTIONS.UP;
+        break;
+      case 40:
+        GAME.PLAYER.direction = DIRECTIONS.DOWN;
+        break;
+    }
+  } else if (GAME.STATE === STATES.RESTART) {
+    switch (e.keyCode) {
+      case 32:
+        playConfirmationSound();
+        GAME.STATE = STATES.PLAY;
+        initGame();
+        window.requestAnimationFrame(loop);
+        break;
+    }
+  }
+}
+
+function update() {
+  if (GAME.STATE === STATES.PLAY) {
+    switch (GAME.PLAYER.direction) {
+      case DIRECTIONS.UP:
+        GAME.PLAYER.y = GAME.PLAYER.y - 2;
+        break;
+      case DIRECTIONS.DOWN:
+        GAME.PLAYER.y = GAME.PLAYER.y + 2;
+        break;
+    }
+    let startBoundary =
+      context.canvas.height / 2 - map.tiles[1].height / 2 + map.tiles[1].offset;
     let endBoundary = startBoundary + map.tiles[1].height;
-    if (player.y + 4 < startBoundary || player.y - 4 > endBoundary) {
-      with (new AudioContext)
-      with (G = createGain())
-      for (i in D = [22, 25, 23, 25, 23])
-        with (createOscillator())
-        if (D[i])
-          connect(G),
-            G.connect(destination),
-            start(i * .1),
-            frequency.setValueAtTime(440 * 1.06 ** (13 - D[i]), i * .1), type = 'square',
-            gain.setValueAtTime(1, i * .1),
-            gain.setTargetAtTime(.0001, i * .1 + .08, .005),
-            stop(i * .1 + .09)
-      GAME.STATE = GAME.STATES.RESTART;
+    if (
+      GAME.PLAYER.y + HIT_LEEWAY < startBoundary ||
+      GAME.PLAYER.y - HIT_LEEWAY > endBoundary
+    ) {
+      with (new AudioContext())
+        with ((G = createGain()))
+          for (i in (D = [22, 25, 23, 25, 23]))
+            with (createOscillator())
+              if (D[i])
+                connect(G),
+                  G.connect(destination),
+                  start(i * 0.1),
+                  frequency.setValueAtTime(440 * 1.06 ** (13 - D[i]), i * 0.1),
+                  (type = "square"),
+                  gain.setValueAtTime(1, i * 0.1),
+                  gain.setTargetAtTime(0.0001, i * 0.1 + 0.08, 0.005),
+                  stop(i * 0.1 + 0.09);
+      GAME.STATE = STATES.RESTART;
     }
   }
 }
 
 function draw() {
   context.clearRect(0, 0, context.canvas.width, context.canvas.height);
-  if (GAME.STATE === GAME.STATES.MENU) {
-    context.textBaseline = 'middle';
+  if (GAME.STATE === STATES.MENU) {
+    context.textBaseline = "middle";
     context.textAlign = "center";
-    context.fillStyle = '#6fea62';
+    context.fillStyle = "#6fea62";
     context.font = "40px Courier New";
-    context.fillText("Press Space to Play!", context.canvas.width / 2, context.canvas.height / 2);
-  } else if (GAME.STATE === GAME.STATES.PLAY) {
+    context.fillText(
+      "Press Space to Play!",
+      context.canvas.width / 2,
+      context.canvas.height / 2
+    );
+  } else if (GAME.STATE === STATES.PLAY) {
     var quadrantWidth = Math.ceil(context.canvas.width / map.tiles.length);
-    map.tiles.forEach(function (quadrant, index) {
-      context.fillStyle = index % 2 ? '#84ed78' : '#6fea62';
-      context.fillRect(quadrantWidth * index, (context.canvas.height / 2 - quadrant.height / 2) + quadrant.offset, quadrantWidth, quadrant.height);
+    map.tiles.forEach(function(quadrant, index) {
+      context.fillStyle = index % 2 ? "#84ed78" : "#6fea62";
+      context.fillRect(
+        quadrantWidth * index,
+        context.canvas.height / 2 - quadrant.height / 2 + quadrant.offset,
+        quadrantWidth,
+        quadrant.height
+      );
     });
-    context.fillStyle = '#19700f';
-    context.fillRect(player.x, player.y, 10, 10);
-    context.fillStyle = '#6fea62';
+    context.fillStyle = "#19700f";
+    context.fillRect(GAME.PLAYER.x, GAME.PLAYER.y, GAME.PLAYER.height, GAME.PLAYER.width);
+    context.fillStyle = "#6fea62";
     context.font = "20px Courier New";
     context.fillText("Score: " + GAME.SCORE, 80, 40);
-  } else if (GAME.STATE === GAME.STATES.RESTART) {
-    context.textBaseline = 'middle';
+  } else if (GAME.STATE === STATES.RESTART) {
+    context.textBaseline = "middle";
     context.textAlign = "center";
-    context.fillStyle = '#6fea62';
+    context.fillStyle = "#6fea62";
     context.font = "40px Courier New";
-    context.fillText("You lost! Score: " + GAME.SCORE, context.canvas.width / 2, context.canvas.height / 2 - 30);
-    context.fillText("Press Space to Restart!", context.canvas.width / 2, context.canvas.height / 2 + 30);
+    context.fillText(
+      "You lost! Score: " + GAME.SCORE,
+      context.canvas.width / 2,
+      context.canvas.height / 2 - 30
+    );
+    context.fillText(
+      "Press Space to Restart!",
+      context.canvas.width / 2,
+      context.canvas.height / 2 + 30
+    );
   }
 }
 
 function loop(timestamp) {
-  var progress = timestamp - lastRender
-
-  update(progress)
-  draw()
-  if (GAME.STATE === GAME.STATES.PLAY) {
-    lastRender = timestamp
-    window.requestAnimationFrame(loop)
+  let progress = timestamp - lastRender;
+  update(progress);
+  draw();
+  if (GAME.STATE === STATES.PLAY) {
+    lastRender = timestamp;
+    window.requestAnimationFrame(loop);
   }
 }
-var lastRender = 0
-
+var lastRender = 0;
 window.requestAnimationFrame(loop);
-
-window.addEventListener('keydown', this.check, false);
-
-function check(e) {
-  if (GAME.STATE === GAME.STATES.MENU) {
-    switch (e.keyCode) {
-      case 32:
-      playConfirmationSound();
-        GAME.STATE = GAME.STATES.PLAY;
-        initGame();
-        window.requestAnimationFrame(loop);
-        break;
-    }
-  } else if (GAME.STATE === GAME.STATES.PLAY) {
-    switch (e.keyCode) {
-      case 38:
-        player.direction = DIRECTIONS.UP;
-        break;
-      case 40:
-        player.direction = DIRECTIONS.DOWN;
-        break;
-    }
-  } else if (GAME.STATE === GAME.STATES.RESTART) {
-    switch (e.keyCode) {
-      case 32:
-      playConfirmationSound();
-        GAME.STATE = GAME.STATES.PLAY;
-        initGame();
-        window.requestAnimationFrame(loop);
-        break;
-    }
-  }
-}
-
-function playConfirmationSound() {
-  with(new AudioContext)
-with(G=createGain())
-for(i in D=[5,7])
-with(createOscillator())
-if(D[i])
-connect(G),
-G.connect(destination),
-start(i*.1),
-frequency.setValueAtTime(440*1.06**(13-D[i]),i*.1),
-gain.setValueAtTime(1,i*.1),
-gain.setTargetAtTime(.0001,i*.1+.08,.005),
-stop(i*.1+.09)
-}
